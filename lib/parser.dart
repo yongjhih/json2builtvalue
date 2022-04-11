e86eece15c68efd44745bb4c4c18428cf54f510a
Andrew Chen <andrew.cc@binance.com>
2022-04-12 00:38:50 +0800

    Update analysis_options.yaml

--- lib/parser.dart
+++ lib/parser.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:built_collection/src/list.dart';
+import 'package:built_collection/src/list.dart' show ListBuilder;
 import 'package:code_builder/code_builder.dart';
 import 'root.dart';
 import 'package:recase/recase.dart';
@@ -9,23 +9,23 @@ import 'package:tuple/tuple.dart';
 class Parser {
 
   String parse(String jsonString, String topLevelName) {
-    var decode = json.decode(jsonString);
+    final decode = json.decode(jsonString);
 
-    List<Tuple2<String, List<Subtype>>> allClasses = [];
+    final List<Tuple2<String, List<Subtype>>> allClasses = [];
 
     //check if list or map
     if (decode is List) {}
 
-    List<Subtype> topLevel = _getTypedClassFields(decode);
-    allClasses.add(new Tuple2(topLevelName, topLevel));
+    final List<Subtype> topLevel = _getTypedClassFields(decode);
+    allClasses.add(Tuple2(topLevelName, topLevel));
 
-    topLevel.forEach((Subtype s) {
+    for (final s in topLevel) {
       if ((s.type == JsonType.LIST && s.listType == JsonType.MAP) ||
           s.type == JsonType.MAP) {
-        var getTypedClassFields = _getTypedClassFields(s.value);
-        allClasses.add(new Tuple2(s.name, getTypedClassFields));
+        final getTypedClassFields = _getTypedClassFields(s.value);
+        allClasses.add(Tuple2(s.name, getTypedClassFields));
       }
-    });
+    }
 
 //    print('all: $allClasses');
 
@@ -33,7 +33,7 @@ class Parser {
 //      _generateStringClass(tuple.item2, tuple.item1 + 'Dto');
 //    });
 
-    String reduce = allClasses
+    final String reduce = allClasses
         .map((tuple) => _generateStringClass(tuple.item2, tuple.item1))
         .reduce((s1, s2) => s1 + s2);
 
@@ -41,98 +41,98 @@ class Parser {
   }
 
   String _generateStringClass(List<Subtype> topLevel, String name) {
-    var topLevelClass = new Class((b) => b
+    final topLevelClass = Class((b) => b
       ..abstract = true
-      ..constructors.add(new Constructor((b) => b..name = '_'))
-      ..implements.add(new Reference(
+      ..constructors.add(Constructor((b) => b..name = '_'))
+      ..implements.add(Reference(
           'Built<${_getPascalCaseClassName(name)}, ${_getPascalCaseClassName(
               name)}Builder>'))
       ..name = _getPascalCaseClassName(name)
       ..methods = _buildMethods(topLevel)
-      ..methods.add(new Method((b) => b
+      ..methods.add(Method((b) => b
         ..name = 'toJson'
-        ..returns = new Reference('String')
-        ..body = new Code(
+        ..returns = const Reference('String')
+        ..body = Code(
             'return json.encode(serializers.serializeWith(${_getPascalCaseClassName(
                 name)}.serializer, this));')))
-      ..methods.add(new Method((b) => b
+      ..methods.add(Method((b) => b
         ..name = 'fromJson'
         ..static = true
-        ..requiredParameters.add(new Parameter((b) => b
+        ..requiredParameters.add(Parameter((b) => b
           ..name = 'jsonString'
-          ..type = new Reference('String')))
-        ..returns = new Reference(_getPascalCaseClassName(name))
-        ..body = new Code(
+          ..type = const Reference('String')))
+        ..returns = Reference(_getPascalCaseClassName(name))
+        ..body = Code(
             'return serializers.deserializeWith(${_getPascalCaseClassName(
                 name)}.serializer, json.decode(jsonString));')))
-      ..methods.add(new Method((b) => b
+      ..methods.add(Method((b) => b
         ..type = MethodType.getter
         ..name = 'serializer'
         ..static = true
         ..lambda = true
         ..returns =
-            new Reference('Serializer<${_getPascalCaseClassName(name)}>')
-        ..body = new Code('_\$${ReCase(name).camelCase}Serializer')))
-      ..constructors.add(new Constructor((b) => b
+            Reference('Serializer<${_getPascalCaseClassName(name)}>')
+        ..body = Code('_\$${ReCase(name).camelCase}Serializer')))
+      ..constructors.add(Constructor((b) => b
         ..factory = true
         ..redirect = refer(' _\$${_getPascalCaseClassName(name)}')
-        ..requiredParameters.add(new Parameter((b) => b
+        ..requiredParameters.add(Parameter((b) => b
         ..defaultTo = Code('= _\$${_getPascalCaseClassName(name)}')
           ..name = '[updates(${_getPascalCaseClassName(name)}Builder b)]'))),)
           );
 
-    String classString = topLevelClass.accept(DartEmitter()).toString();
+    final String classString = topLevelClass.accept(DartEmitter()).toString();
 
-    String header = """
-library ${new ReCase(name).snakeCase};
+    final String header = """
+library ${ReCase(name).snakeCase};
 import 'dart:convert';
 
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 
-part '${new ReCase(name).snakeCase}.g.dart';
+part '${ReCase(name).snakeCase}.g.dart';
 """;
 
-    String output = "$header\n$classString";
+    final String output = "$header\n$classString";
 
 //    print(output);
     return output;
   }
 
-  String _getPascalCaseClassName(String name) => new ReCase(name).pascalCase;
+  String _getPascalCaseClassName(String name) => ReCase(name).pascalCase;
 
   ListBuilder<Method> _buildMethods(List<Subtype> topLevel) {
-    return new ListBuilder(topLevel.map((Subtype s) => new Method((b) => b
-      ..name = new ReCase(s.name).camelCase
+    return ListBuilder(topLevel.map((Subtype s) => Method((b) => b
+      ..name = ReCase(s.name).camelCase
       ..returns = _getDartType(s)
-      ..annotations.add(new CodeExpression(
-          new Code("BuiltValueField(wireName: '${s.name}')")))
+      ..annotations.add(CodeExpression(
+          Code("BuiltValueField(wireName: '${s.name}')")))
       ..type = MethodType.getter)));
   }
 
   Reference _getDartType(Subtype subtype) {
-    JsonType type = subtype.type;
+    final JsonType type = subtype.type;
     switch (type) {
       case JsonType.INT:
-        return new Reference('int?');
+        return const Reference('int?');
       case JsonType.DOUBLE:
-        return new Reference('double');
+        return const Reference('double?');
       case JsonType.BOOL:
-        return new Reference('bool');
+        return const Reference('bool?');
       case JsonType.STRING:
-        return new Reference('String');
+        return const Reference('String?');
       case JsonType.MAP:
-        return new Reference(new ReCase(subtype.name).pascalCase);
+        return Reference("${ReCase(subtype.name).pascalCase}?");
       case JsonType.LIST:
-        return new Reference('BuiltList<${_getDartTypeFromJsonType(subtype)}>');
+        return Reference('BuiltList<${_getDartTypeFromJsonType(subtype)}>?');
       default:
-        return new Reference('dynamic');
+        return const Reference('dynamic');
     }
   }
 
   String _getDartTypeFromJsonType(Subtype subtype) {
-    var type = subtype.listType;
+    final type = subtype.listType;
     switch (type) {
       case JsonType.INT:
         return 'int';
@@ -141,62 +141,62 @@ part '${new ReCase(name).snakeCase}.g.dart';
       case JsonType.STRING:
         return 'String';
       case JsonType.MAP:
-        return new ReCase(subtype.name).pascalCase;
+        return ReCase(subtype.name).pascalCase;
       default:
         return 'dynamic';
     }
   }
 
-  List<Subtype> _getTypedClassFields(decode) {
-    List<Subtype> topLevelClass = [];
-    var toDecode;
+  List<Subtype> _getTypedClassFields(dynamic decode) {
+    final List<Subtype> topLevelClass = [];
+    late final Map<String, dynamic>? toDecode;
 
-    if (decode is List) {
+    if (decode is List<Map<String, dynamic>>) {
       toDecode = decode[0];
-    } else {
+    } else if (decode is Map<String, dynamic>) {
       toDecode = decode;
     }
 
-//  if (toDecode is Map) {
-    toDecode.forEach((key, val) {
+    toDecode?.forEach((key, val) {
       topLevelClass.add(_returnType(key, val));
     });
-//  }
     return topLevelClass;
   }
 
-  Subtype _returnType(key, val) {
-    if (val is String)
-      return new Subtype(key, JsonType.STRING, val);
-    else if (val is int)
-      return new Subtype(key, JsonType.INT, val);
-    else if (val is num)
-      return new Subtype(key, JsonType.DOUBLE, val);
-    else if (val is bool)
-      return new Subtype(key, JsonType.BOOL, val);
-    else if (val is List) {
-      return new Subtype(key, JsonType.LIST, val,
+  Subtype _returnType(String key, dynamic val) {
+    if (val is String) {
+      return Subtype(key, JsonType.STRING, val);
+    } else if (val is int) {
+      return Subtype(key, JsonType.INT, val);
+    } else if (val is num) {
+      return Subtype(key, JsonType.DOUBLE, val);
+    } else if (val is bool) {
+      return Subtype(key, JsonType.BOOL, val);
+    } else if (val is List) {
+      return Subtype(key, JsonType.LIST, val,
           listType: _returnJsonType(val));
     } else if (val is Map) {
-      return new Subtype(key, JsonType.MAP, val);
-    } else
-      throw new ArgumentError('Cannot resolve JSON-encodable type for $val.');
+      return Subtype(key, JsonType.MAP, val);
+    } else {
+      throw ArgumentError('Cannot resolve JSON-encodable type for $val.');
+    }
   }
 
   JsonType _returnJsonType(List list) {
-    var item = list[0];
+    final item = list[0];
     print('got item $item');
-    if (item is String)
+    if (item is String) {
       return JsonType.STRING;
-    else if (item is int)
+    } else if (item is int) {
       return JsonType.INT;
-    else if (item is num)
+    } else if (item is num) {
       return JsonType.DOUBLE;
-    else if (item is bool)
+    } else if (item is bool) {
       return JsonType.BOOL;
-    else if (item is Map)
+    } else if (item is Map) {
       return JsonType.MAP;
-    else
-      throw new ArgumentError('Cannot resolve JSON-encodable type for $item.');
+    } else {
+      throw ArgumentError('Cannot resolve JSON-encodable type for $item.');
+    }
   }
 }
